afc49f7903e7c153ef330afd51787073d3ea1af7
MadhavanAR <amadhav693@gmail.com>
2024-12-24 19:07:38 +0530

    Updated source

---
+++ src/ContactsContext.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ContactsContext = React.createContext({
+  contactsList: [],
+  recentCalls: [],
+  AddContact: () => {},
+  onToggleSelectContact: () => {},
+  toggleSelectAllContacts: () => {},
+  onDeleteContacts: () => {},
+  updateContact: () => {},
+  updateRecentCalls: () => {},
+});
+
+export default ContactsContext;
